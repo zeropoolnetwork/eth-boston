645776646118953606ae115aec4f8278d2285f19
artall64 <artall64@gmail.com>
2019-09-07 19:02:58 +0300

    update smart mock

--- contract/contract_test.go
+++ contract/contract_test.go
@@ -8,27 +8,31 @@ import (
 )
 
 func TestSend(t *testing.T){
-	sm, err := ConnectContract("https://rinkeby.infura.io", "0x6fa5e3cb3d351f4ff8529e97d334323d6fe01d1a")
+	sm, err := ConnectContract("https://rinkeby.infura.io", "0x43218f7958c72a123cde8f56a3b5b604a934d455")
 	if err != nil{
 		t.Error(err)
+		return
 	}
 
 	fp :=  big.NewInt(1)
 	sp := [32]byte{}
 
-	data, err := sm.GetDataByteCode("test",fp,fp,fp,fp,fp,fp,fp,fp,sp,sp,sp,sp,sp,sp)
+	data, err := sm.GetDataByteCode("setData",fp,fp,fp,fp,fp,fp,fp,fp,sp,sp,sp,sp,sp,sp)
 	if err != nil{
 		t.Error(err)
+		return
 	}
 
-	tx, err := sm.SignTransaction(os.Getenv("PRIVATE_KEY"), big.NewInt(0), data)
+	tx, err := sm.SignTransaction(os.Getenv("PRIVATE_KEY"), big.NewInt(20), data)
 	if err != nil {
 		t.Error(err)
+		return
 	}
 
 	txHash, err := sm.SendSignedTransaction(tx)
 	if err != nil{
 		t.Error(err)
+		return
 	}
 
 	log.Println(txHash)
