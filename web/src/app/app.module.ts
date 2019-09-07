78750da9b3731f5c999b890902abaa368a2546dd
Nick Kozlov <nk@buttonwallet.com>
2019-09-07 01:12:01 -0400

    add elements

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { HeadComponent } from './head/head.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AddressAssetsComponent } from './address-assets/address-assets.component';
+import { TransferComponent } from './transfer/transfer.component';
+import { DepositComponent } from './deposit/deposit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadComponent,
     DashboardComponent,
-    AddressAssetsComponent
+    AddressAssetsComponent,
+    TransferComponent,
+    DepositComponent
   ],
   imports: [
     BrowserModule,
