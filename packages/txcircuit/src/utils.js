c9852c9bf810d64692ba7ae4a5c2f7aa1222520e
Igor Gulamov <igor.gulamov@gmail.com>
2019-09-08 03:09:09 -0400

    fix websnark

--- packages/txcircuit/src/utils.js
+++ packages/txcircuit/src/utils.js
@@ -76,4 +76,51 @@ function pubkey(pk) {
 }
 
 
-_.assign(exports, { randrange, pedersen, witness, fload, verify, pubkey });
+function linearize_vk_verifier(vk_verifier) {
+  const result = Array(14 + 2 * vk_verifier.IC.length);
+  result[0] = vk_verifier.vk_alfa_1[0];
+  result[1] = vk_verifier.vk_alfa_1[1];
+
+  result[2] = vk_verifier.vk_beta_2[0][1];
+  result[3] = vk_verifier.vk_beta_2[0][0];
+  result[4] = vk_verifier.vk_beta_2[1][1];
+  result[5] = vk_verifier.vk_beta_2[1][0];
+
+  result[6] = vk_verifier.vk_gamma_2[0][1];
+  result[7] = vk_verifier.vk_gamma_2[0][0];
+  result[8] = vk_verifier.vk_gamma_2[1][1];
+  result[9] = vk_verifier.vk_gamma_2[1][0];
+
+  result[10] = vk_verifier.vk_delta_2[0][1];
+  result[11] = vk_verifier.vk_delta_2[0][0];
+  result[12] = vk_verifier.vk_delta_2[1][1];
+  result[13] = vk_verifier.vk_delta_2[1][0];
+
+  for (let i = 0; i < vk_verifier.IC.length; i++) {
+    result[14 + 2 * i] = vk_verifier.IC[i][0];
+    result[14 + 2 * i + 1] = vk_verifier.IC[i][1];
+  }
+  return result;
+}
+
+function linearize_proof(proof) {
+  const result = Array(8);
+  result[0] = proof.pi_a[0];
+  result[1] = proof.pi_a[1];
+
+  result[2] = proof.pi_b[0][1];
+  result[3] = proof.pi_b[0][0];
+  result[4] = proof.pi_b[1][1];
+  result[5] = proof.pi_b[1][0];
+
+  result[6] = proof.pi_c[0];
+  result[7] = proof.pi_c[1];
+
+  return result;
+}
+
+
+
+
+
+_.assign(exports, { randrange, pedersen, witness, fload, verify, pubkey, linearize_vk_verifier, linearize_proof, proof });
