806100bb9ce37163592c5d3af6cd39a8ed70911d
Nick Kozlov <nk@buttonwallet.com>
2019-09-07 21:32:50 -0400

    fixes

--- packages/MiMCTree/truffle-config.js
+++ packages/MiMCTree/truffle-config.js
@@ -25,7 +25,7 @@ module.exports = {
       network_id: '*', // eslint-disable-line camelcase
     },
     ropsten: {
-      provider: new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/", 0, 10),
+      provider: new HDWalletProvider(process.env.PRIVATE_KEY, "https://ropsten.infura.io/"),
       network_id: 3, // eslint-disable-line camelcase
       skipDryRun: true
     },
@@ -47,23 +47,28 @@ module.exports = {
       network_id: '*', // eslint-disable-line camelcase
     },
     rinkeby: {
-      provider: new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/", 0, 10),
+      provider: new HDWalletProvider(process.env.PRIVATE_KEY, "https://rinkeby.infura.io/"),
       network_id: 4,
       skipDryRun: true
     },
+    kovan: {
+      provider: new HDWalletProvider("e6d304fa1be77229d017b21156265f910a2f4db98a09c39d3f2f18d75639fd5e", "https://kovan.infura.io/v3/c7463beadf2144e68646ff049917b716"),
+      network_id: 42,
+      skipDryRun: true
+    },
     sokol: {
-      provider: new HDWalletProvider(process.env.MNEMONIC, "https://sokol.poa.network/", 0, 10),
+      provider: new HDWalletProvider(process.env.PRIVATE_KEY, "https://sokol.poa.network/"),
       network_id: "*", // Match any network id
       skipDryRun: true
     },
     skale: {
-      provider: new HDWalletProvider(process.env.MNEMONIC, "http://157.230.154.5:8134/", 0, 10),
+      provider: new HDWalletProvider(process.env.PRIVATE_KEY, "http://157.230.154.5:8134/"),
       gasPrice: 0,
       network_id: "*",
       skipDryRun: true
     },
     thunder: {
-      provider: new HDWalletProvider(process.env.MNEMONIC, "https://mainnet-rpc.thundercore.com/"),
+      provider: new HDWalletProvider(process.env.PRIVATE_KEY, "https://mainnet-rpc.thundercore.com/"),
       network_id: "*",
       gas: 3000000,
       gasPrice: 20000000000
