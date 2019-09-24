7eea2fc976d8971eb5990d5aecfa574f4a411596
artem <artall64@gmail.com>
2019-09-24 22:17:42 +0300

    logs and formatting

--- web/src/app/withdraw/withdraw.component.ts
+++ web/src/app/withdraw/withdraw.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Web3Service } from "../web3.service";
 
 @Component({
@@ -12,7 +12,7 @@ export class WithdrawComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.withdrawal()
+    this.withdrawal();
   }
 
   withdrawal() {
@@ -27,16 +27,15 @@ export class WithdrawComponent implements OnInit {
     console.log(`Private key: ${pvk}`);
     console.log(`Public key: ${pbk}`);
 
-    let encryptedUtxo = w.hexToBigInt("27fc96c235b9409c23c6664282549eb5bfe14a9a56d61d54af1479c07390241642ffafaf6c283c4c8c8ea71665ba7d7c3ab32ebf04f933f52af1850e432c2d28b8773e4c34595013e4b6d0601b8fa19e00f8e3c85e5df564a3e9b5817edcee0caa2b2b6bd41ed115d6344b09ac7b22f7c4b16b0fa76856b565b8bbdabd223e205fbadcf58cead6246242ffe857d549681981ee67b640ffd1bacfc3ec78fc8d34");
+    // tslint:disable-next-line:max-line-length
+    const encryptedUtxo = w.hexToBigInt("27fc96c235b9409c23c6664282549eb5bfe14a9a56d61d54af1479c07390241642ffafaf6c283c4c8c8ea71665ba7d7c3ab32ebf04f933f52af1850e432c2d28b8773e4c34595013e4b6d0601b8fa19e00f8e3c85e5df564a3e9b5817edcee0caa2b2b6bd41ed115d6344b09ac7b22f7c4b16b0fa76856b565b8bbdabd223e205fbadcf58cead6246242ffe857d549681981ee67b640ffd1bacfc3ec78fc8d34");
     console.log(`Encrypted utxo: ${encryptedUtxo}`);
 
-    let decryptedUtxo = w.Crypto.decrypt(encryptedUtxo, pbk, pvk.k);
-    console.log("Decrypted utxo");
-    console.log(decryptedUtxo);
+    const decryptedUtxo = w.Crypto.decrypt(encryptedUtxo, pbk, pvk.k);
+    console.log("Decrypted utxo:", decryptedUtxo);
 
-    let readableUtxo = w.snark.utxo(decryptedUtxo[0], decryptedUtxo[1], decryptedUtxo[2], decryptedUtxo[3]);
-    console.log("Readable utxo");
-    console.log(readableUtxo);
+    const readableUtxo = w.snark.utxo(decryptedUtxo[0], decryptedUtxo[1], decryptedUtxo[2], decryptedUtxo[3]);
+    console.log("Readable utxo:", readableUtxo);
 
     const amountToWithdrawal = readableUtxo.amount;
     console.log(`Amount to withdrawal: ${amountToWithdrawal}`);
@@ -54,17 +53,21 @@ export class WithdrawComponent implements OnInit {
         const publicInputs = data[0];
         console.log("Public inputs");
         console.log(publicInputs);
+
         const proof = data[1];
         console.log("Proof");
         console.log(proof);
+
         const cyphertext1 = data[2];
         console.log(`Cyphertext1: ${cyphertext1}`);
+
         const cyphertext2 = data[3];
         console.log(`Cyphertext2: ${cyphertext2}`);
 
         this.web3.kovan.withdrawal(publicInputs, proof, cyphertext1, cyphertext2).then((txHash) => {
           console.log(`Transaction hash: ${txHash}`);
         });
+
       });
 
   }
